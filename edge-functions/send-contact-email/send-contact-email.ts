a794edc980abb991f4761a320552c98503360863
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-02-01 18:32:27 +0000

    Update

--- edge-functions/send-contact-email/send-contact-email.ts
+++ edge-functions/send-contact-email/send-contact-email.ts
@@ -1,5 +1,21 @@
 import type { Context } from "https://edge.netlify.com";
 
-export default async (request: Request, context: Context) => {
-  return Response.json({ hello: "world", location: context.geo.city });
+export default async (request: Request, _context: Context) => {
+  const SENDGRID_API_KEY = Deno.env.get("SENDGRID_API_KEY");
+  const formData = (await request.formData())
+  const name = formData.get('Name')
+  const subject = formData.get('Subject')
+  const email = formData.get('Email')
+  const message = formData.get('Message')
+
+  console.log(SENDGRID_API_KEY);
+
+  return Response.json(
+    {
+      name: name,
+      email: email,
+      subject: subject,
+      message: message,
+    }
+  );
 };
