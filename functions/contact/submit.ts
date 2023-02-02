f303b5c8cd7cd73db6a6660a31ca0d272a2e025e
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-02-02 13:44:48 +0000

    Initial implementation

--- functions/contact/submit.ts
+++ functions/contact/submit.ts
@@ -1,14 +1,80 @@
+interface MailParams {
+  dest: {
+    email: string,
+    name: string,
+  },
+  from?: {
+    name: string,
+    email: string,
+  }
+  subject: string,
+  message: string,
+}
+
+const NOREPLY_FROM = {
+  name: 'Mircea Anton - mailbot',
+  email: 'noreply@mirceanton.com',
+}
+const CONTACT_EMAIL = {
+  name: "Mircea Anton",
+  email: "contact@mirceanton.com",
+}
+
 export async function onRequestPost({ request, env }) {
+  // Extract formData from the request and parse out the form fields
   const formData = await request.formData();
-  const name = formData.get("Name")
-  const email = formData.get("Email")
+  const person = {
+    name: formData.get("Name"),
+    email: formData.get("Email")
+  }
   const subject = formData.get("Subject")
   const message = formData.get("Message")
 
-  return Response.json({
-    name: name,
-    email: email,
-    subject: subject,
-    message: message
+  // Send notification mail to user
+  await sendMail({
+    dest: person,
+    subject: "Thank you for contacting me!",
+    message: `
+      Hello, ${person.name},
+
+      Thank you for taking the time to get in touch. This is an automated email sent to notify you that I have received your email.
+      I will read it and reply as soon as I possibly can!
+
+      Thank you,
+      Mircea ANTON
+    `,
   })
+
+  // Send mail to me
+  await sendMail({
+    dest: CONTACT_EMAIL,
+    subject: "Form Submission: " + subject,
+    message: message,
+  })
+
+  return Response.redirect('https://mirceanton.com/')
+}
+
+async function sendMail({dest, from, subject, message}: MailParams) {
+  return await new Request('https://api.mailchannels.net/tx/v1/send', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      personalizations: [
+        {
+          to: [dest],
+        },
+      ],
+      from: from ?? NOREPLY_FROM,
+      subject: subject,
+      content: [
+        {
+          type: 'text/plain',
+          value: message,
+        },
+      ],
+    }),
+  });
 }
