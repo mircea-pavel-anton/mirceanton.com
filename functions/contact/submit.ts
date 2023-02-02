9061fa3f9af9df06afcda0fd3374aaa6cb0035fe
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-02-02 13:06:24 +0000

    Extract form data.

--- functions/contact/submit.ts
+++ functions/contact/submit.ts
@@ -1,7 +1,14 @@
-interface Env {
-  KV: KVNamespace;
-}
+export async function onRequestPost({ request, env }) {
+  const formData = await request.formData();
+  const name = formData.get("Name")
+  const email = formData.get("Email")
+  const subject = formData.get("Subject")
+  const message = formData.get("Message")
 
-export const onRequest: PagesFunction<Env> = async (context) => {
-  return new Response("Hello world");
+  return Response.json({
+    name: name,
+    email: email,
+    subject: subject,
+    message: message
+  })
 }
