cb71450398b81c064cd42444f3d50367058807b6
Mircea-Pavel Anton <contact@mirceanton.com>
2023-09-10 20:57:32 +0000

    Add a catch-all rule for all other emails (needs to be tested)

--- infrastructure/cloudflare/email_routing.tf
+++ infrastructure/cloudflare/email_routing.tf
@@ -13,3 +13,18 @@ resource "cloudflare_email_routing_rule" "no_reply_drop" {
     type  = "drop"
   }
 }
+
+resource "cloudflare_email_routing_catch_all" "catch_all_forward" {
+  zone_id = cloudflare_zone.mirceanton_com.id
+  name    = "Catch all emails and forward to GMail"
+  enabled = true
+
+  matcher {
+    type = "all"
+  }
+
+  action {
+    type  = "forward"
+    value = ["mircea.pavel.anton@gmail.com"]
+  }
+}
