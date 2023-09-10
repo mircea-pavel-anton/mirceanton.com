bebb147ffed550062ad84893cdbcef04d4711605
Mircea-Pavel Anton <contact@mirceanton.com>
2023-09-10 22:07:19 +0000

    Move terraform files to `infrastructure/`

--- infrastructure/cloudflare/pages.tf
+++ infrastructure/cloudflare/pages.tf
@@ -1,3 +1,9 @@
+resource "cloudflare_pages_project" "mirceanton" {
+  account_id        = var.cloudflare_account_id
+  name              = "mirceanton"
+  production_branch = "main"
+}
+
 resource "cloudflare_pages_domain" "mirceanton-com" {
   account_id   = var.cloudflare_account_id
   project_name = "mirceanton"
