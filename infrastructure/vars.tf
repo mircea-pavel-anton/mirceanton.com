b2ae52416a4924b2a3e5e7ac03d7b557c7d522f8
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-09-13 21:11:41 +0000

    Migrate to email+api_key for auth

--- infrastructure/vars.tf
+++ infrastructure/vars.tf
@@ -2,6 +2,12 @@ variable "cloudflare_account_id" {
   description = "Account ID for CloudFlare"
 }
 
-variable "cloudflare_api_token" {
-  description = "CloudFlare API Token"
+variable "cloudflare_email" {
+  type        = string
+  description = "clouflare email address"
+}
+
+variable "cloudflare_api_key" {
+  type        = string
+  description = "cloudflare api key"
 }
