456d7694ca0c767aa26f960fb4a422e3752b9772
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-09-13 21:10:52 +0000

    Create target email address

--- infrastructure/cloudflare_emails.tf
+++ infrastructure/cloudflare_emails.tf
@@ -5,3 +5,11 @@ resource "cloudflare_email_routing_settings" "email_routing_enable" {
   zone_id = cloudflare_zone.zone.id
   enabled = "true"
 }
+
+# =================================================================================================
+# Email Routing Target -> personal Gmail
+# =================================================================================================
+resource "cloudflare_email_routing_address" "gmail" {
+  account_id = var.cloudflare_account_id
+  email      = "mircea.pavel.anton@gmail.com"
+}
