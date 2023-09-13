7c759279ec6d9d89fce7a2aee59326fb7d5a394f
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-09-13 21:11:08 +0000

    Set up email forwarders

--- infrastructure/cloudflare_emails.tf
+++ infrastructure/cloudflare_emails.tf
@@ -13,3 +13,31 @@ resource "cloudflare_email_routing_address" "gmail" {
   account_id = var.cloudflare_account_id
   email      = "mircea.pavel.anton@gmail.com"
 }
+
+# =================================================================================================
+# Email Routing Rule: Forward emails to my personal gmail
+# =================================================================================================
+resource "cloudflare_email_routing_rule" "gmail_forwarder" {
+  for_each = toset([
+    "personal@mirceanton.com",
+    "contact@mirceanton.com",
+    "mircea@mirceanton.com",
+    "me@mirceanton.com",
+    "spam@mirceanton.com"
+  ])
+
+  zone_id = cloudflare_zone.zone.id
+  name    = "Gmail Forwarder"
+  enabled = true
+
+  matcher {
+    type  = "literal"
+    field = "to"
+    value = each.key
+  }
+
+  action {
+    type  = "forward"
+    value = ["mircea.pavel.anton@gmail.com"]
+  }
+}
